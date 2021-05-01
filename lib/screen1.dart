47bc78fc1f0f5456fc94c4e3db95a4eb5519bdfa
Srikhar Shashi D <dasojusrikhar@gmail.com>
2021-05-02 00:31:24 +0530

    Day 2 Done

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -11,26 +11,33 @@ class _Screen1State extends State<Screen1> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-
-      body: ListView(
-        children: [
-        Padding(child: Text("Good Evening !", style: TextStyle(color: txt_color,fontSize: 30,letterSpacing: 0.5),),padding: EdgeInsets.all(15),),
-          Cont1(),
-
-        ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: ListView(
+          children: [
+            Padding(child: Text("Good Evening !", style: TextStyle(color: txt_color, fontSize: 30, letterSpacing: 0.5),), padding: EdgeInsets.all(15),),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:15.0),
+              child: Text("Playlists",style: TextStyle(color:Colors.grey[500],fontSize: 20,letterSpacing: 0.5),),
+            ),
+            Playlists(),
+            Padding(child: Text("Top Artists", style: TextStyle(color: Colors.grey[500], fontSize: 20, letterSpacing: 0.5),), padding: EdgeInsets.all(15),),
+            Cont1(),
+          ],
+        ),
       ),
     );
   }
 }
+
 class Cont1 extends StatefulWidget {
   @override
   _Cont1State createState() => _Cont1State();
 }
 
 class _Cont1State extends State<Cont1> {
-  var txt_color=Colors.white;
+  var txt_color = Colors.white;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +50,7 @@ class _Cont1State extends State<Cont1> {
           children: [
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal:5.0),
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
                 child: Column(
                   //Column1
                   children: [
@@ -56,7 +63,7 @@ class _Cont1State extends State<Cont1> {
                         decoration: BoxDecoration(
                             // border: Border.all(color: txt_color),
                             borderRadius: BorderRadius.circular(20),
-                           color: Colors.grey),
+                            color: Colors.grey),
                         child: Row(
                           children: [
                             Expanded(
@@ -65,7 +72,6 @@ class _Cont1State extends State<Cont1> {
                                   decoration: BoxDecoration(
                                     border: Border.all(color: txt_color),
                                   ),
-
                                 )),
                             Expanded(
                               flex: 3,
@@ -113,7 +119,6 @@ class _Cont1State extends State<Cont1> {
                     Expanded(
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: 5),
-
                         height: 60,
                         child: Row(
                           children: [
@@ -136,7 +141,8 @@ class _Cont1State extends State<Cont1> {
                         ),
                         decoration: BoxDecoration(
                             // border: Border.all(color: txt_color),
-                            borderRadius: BorderRadius.circular(20),color: Colors.grey),
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey),
                       ),
                     ),
                   ],
@@ -145,14 +151,13 @@ class _Cont1State extends State<Cont1> {
             ),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal:8.0),
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
                 child: Column(
                   //Coulmn2
                   children: [
                     Expanded(
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: 5),
-
                         height: 60,
                         child: Row(
                           children: [
@@ -174,8 +179,8 @@ class _Cont1State extends State<Cont1> {
                           ],
                         ),
                         decoration: BoxDecoration(
-                            // border: Border.all(color: txt_color),
-                            borderRadius: BorderRadius.circular(20),
+                          // border: Border.all(color: txt_color),
+                          borderRadius: BorderRadius.circular(20),
                           color: Colors.grey,
                         ),
                       ),
@@ -183,7 +188,6 @@ class _Cont1State extends State<Cont1> {
                     Expanded(
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: 5),
-
                         height: 60,
                         child: Row(
                           children: [
@@ -205,8 +209,8 @@ class _Cont1State extends State<Cont1> {
                           ],
                         ),
                         decoration: BoxDecoration(
-                            // border: Border.all(color: txt_color),
-                            borderRadius: BorderRadius.circular(20),
+                          // border: Border.all(color: txt_color),
+                          borderRadius: BorderRadius.circular(20),
                           color: Colors.grey,
                         ),
                       ),
@@ -214,7 +218,6 @@ class _Cont1State extends State<Cont1> {
                     Expanded(
                       child: Container(
                         margin: EdgeInsets.symmetric(vertical: 5),
-
                         height: 60,
                         child: Row(
                           children: [
@@ -236,12 +239,12 @@ class _Cont1State extends State<Cont1> {
                           ],
                         ),
                         decoration: BoxDecoration(
-                            // border: Border.all(color: txt_color),
-                            borderRadius: BorderRadius.circular(20),
-                        color: Colors.grey,),
+                          // border: Border.all(color: txt_color),
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey,
+                        ),
                       ),
                     ),
-
                   ],
                 ),
               ),
@@ -253,3 +256,84 @@ class _Cont1State extends State<Cont1> {
   }
 }
 
+class Playlists extends StatefulWidget {
+  @override
+  _PlaylistsState createState() => _PlaylistsState();
+}
+
+class _PlaylistsState extends State<Playlists> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        height: 160,
+        decoration: BoxDecoration(border: Border.all(color: Colors.white)),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 130,
+                    width: 130,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.play_arrow,size: 50,color: Colors.white,),
+                        SizedBox(height: 10,),
+                        Text("Top Tracks \n  -Youtube",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight:FontWeight.bold))
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(colors: [Colors.red,Colors.redAccent])),
+                  ),
+                ),
+                Container(
+                    height: 130,
+                    width: 130,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(colors: [Colors.green,Colors.greenAccent])),
+                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.play_arrow,size: 50,color: Colors.white,),
+                      SizedBox(height: 10,),
+                      Text("Top Tracks \n  -Spotify",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight:FontWeight.bold))
+                    ],
+                  ),
+
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 130,
+                    width: 130,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(20),
+
+                   ),
+                  ),
+                ),
+                   Container(
+                    height: 130,
+                    width: 130,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
